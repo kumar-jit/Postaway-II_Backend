c06bbe279e978370801b05e44731c05a7bf74cf9
Kumarjit <63392425+kumar-jit@users.noreply.github.com>
2024-09-20 12:06:15 +0530

    post fdeatures

--- src/features/post/post.controller.js
+++ src/features/post/post.controller.js
@@ -1,9 +1,15 @@
 
-import { createPost, deletePost, getAllPosts, getPostById, getUserPosts, updatePost } from "./post.repository"
+import { ErrorHandler } from "../../utils/errorHandler.js"
+import { createPostRepository, deletePostRepository, getAllPostsRepository, getPostByIdRepository, getUserPostsRepository, updatePostRepository } from "./post.repository.js"
 
 
-export const getAllPost = async (req, res, next) => {
-    
+export const getAllPosts = async (req, res, next) => {
+    try {
+        const allPosts = await getAllPostsRepository();
+        res.status(200).json(allPosts); 
+    } catch (error) {
+        next(new ErrorHandler(500,error));
+    }
     
 }
 
@@ -18,17 +24,18 @@ export const getUserPosts = async (req, res, next) => {
 export const addPost = async (req, res, next) => {
     try {
         const {caption,image, owner} = req.body;
+        if(!caption){
+            return next(new ErrorHandler(401,"Caption is require"));
+        }
         const postData = {
             caption: caption,
-            image: image,
+            image: image || "",
             owner: owner || req.userId
         }
-        const post = await createPost(postData);
-        if(!post){
-            
-        }
+        const post = await createPostRepository(postData);
+        return res.status(201).json(post);
     } catch (error) {
-        
+        next(new ErrorHandler(500, error));
     }
 }
 
--- src/features/post/post.routes.js
+++ src/features/post/post.routes.js
@@ -1,11 +1,11 @@
-import { addPost, getAllPost, getPostDetails, getUserPosts, deletePost, updatePost } from './post.controller.js';
+import { addPost, getAllPosts, getPostDetails, getUserPosts, deletePost, updatePost } from './post.controller.js';
 import authenticateURL from "../../middlewares/jwtAuthorizationMiddleware.js";
 import express from 'express';
 
 
 export const postRouter = express.Router();
 
-postRouter.route("/all").get(authenticateURL,getAllPost);
+postRouter.route("/all").get(authenticateURL,getAllPosts);
 postRouter.route("/:postId").get(authenticateURL,getPostDetails);
 postRouter.route("/user/:userId").get(authenticateURL,getUserPosts);
 postRouter.route("/").post(authenticateURL,addPost);
